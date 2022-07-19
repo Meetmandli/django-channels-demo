cf9474d2343309f21b25026cf23443ea5d2fa7c7
Meetmandli <mmandali@codal.com>
2022-07-19 15:05:30 +0530

    changes in consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -33,6 +33,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'type': 'chat_message',
                 'message': message
             }
+            
         )
 
     # Receive message from room group
